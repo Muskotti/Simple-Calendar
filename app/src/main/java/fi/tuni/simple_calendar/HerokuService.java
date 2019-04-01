79022462b176a99a4ace393ec7b21986e7e5304b
Toni Vänttinen <toni.vanttinen@tuni.fi>
2019-04-01 16:26:17 +0300

    Calendar view
    Add: calendar view with clickable dates

--- app/src/main/java/fi/tuni/simple_calendar/HerokuService.java
+++ app/src/main/java/fi/tuni/simple_calendar/HerokuService.java
@@ -5,6 +5,6 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface HerokuService {
-    @GET("hello")
+    @GET("/")
     Call<ResponseBody> hello();
 }
--- app/src/main/java/fi/tuni/simple_calendar/MainActivity.java
+++ app/src/main/java/fi/tuni/simple_calendar/MainActivity.java
@@ -2,17 +2,8 @@ package fi.tuni.simple_calendar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
-import java.io.IOException;
-
-import okhttp3.ResponseBody;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
+import android.util.Log;
+import android.widget.CalendarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,37 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final TextView textView = (TextView) findViewById(R.id.textView);
-        final Button button = (Button) findViewById(R.id.button);
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://simple-calendar-17tikoot.herokuapp.com/")
-                .build();
 
-        final HerokuService service = retrofit.create(HerokuService.class);
-
-        button.setOnClickListener(new View.OnClickListener() {
+        CalendarView calendarView = findViewById(R.id.simpleCalendarView);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
-            public void onClick(View v) {
-                Call<ResponseBody> call = service.hello();
-                call.enqueue(new Callback<ResponseBody>() {
-                    @Override
-                    public void onResponse(Call<ResponseBody> _,
-                                           Response<ResponseBody> response) {
-                        try {
-                            textView.setText(response.body().string());
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                            textView.setText(e.getMessage());
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(Call<ResponseBody> _, Throwable t) {
-                        t.printStackTrace();
-                        textView.setText(t.getMessage());
-                    }
-                });
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                Log.d("toimi","toimi");
             }
         });
+
     }
 }
