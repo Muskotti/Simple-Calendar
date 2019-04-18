f996efea08a0ed3c983f9b60827a32c77797e701
Toni Vänttinen <toni.vanttinen@tuni.fi>
2019-04-18 17:33:00 +0300

    Get data from heroku server

--- app/src/main/java/fi/tuni/simple_calendar/MainActivity.java
+++ app/src/main/java/fi/tuni/simple_calendar/MainActivity.java
@@ -1,9 +1,21 @@
 package fi.tuni.simple_calendar;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 
 public class MainActivity extends AppCompatActivity implements dateSelected {
 
@@ -17,11 +29,50 @@ public class MainActivity extends AppCompatActivity implements dateSelected {
     public void itemSelected(int year, int month, int dayOfMonth) {
         simpleEventViewFragment ef = (simpleEventViewFragment) getSupportFragmentManager().findFragmentById(R.id.eventFragment);
 
-        //TODO: get selected days items
-        ArrayList<eventItem> eventItemsList = new ArrayList<>();
-        eventItemsList.add(new eventItem(0,"qweqweqwe","qweqweqwe"));
-        eventItemsList.add(new eventItem(1, "qweqweqweqweqw", "qweewqqweqwewqeqe"));
+        //TODO: get data from selected date
 
-        ef.setList(eventItemsList);
+        AsyncTask async = new AsyncTask() {
+
+            ArrayList<eventItem> eventItemsList = new ArrayList<>();
+            simpleEventViewFragment ef = (simpleEventViewFragment) getSupportFragmentManager().findFragmentById(R.id.eventFragment);
+
+            @Override
+            protected Object doInBackground(Object[] objects) {
+                OkHttpClient client = new OkHttpClient();
+                Request request = new Request.Builder()
+                        .url("https://desolate-mesa-25154.herokuapp.com/db")
+                        .build();
+
+                Response response = null;
+
+                try {
+                    response = client.newCall(request).execute();
+
+                    String jsonData = response.body().string();
+                    JSONArray Jarray = new JSONArray(jsonData);
+
+                    for (int i=0; i<Jarray.length(); i++) {
+                        JSONObject actor = Jarray.getJSONObject(i);
+                        String text = actor.getString("eventText");
+                        String name = actor.getString("makerName");
+                        String id = actor.getString("eventId");
+                        eventItemsList.add(new eventItem(Integer.parseInt(id),name,text));
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Object o) {
+                ef.setList(eventItemsList);
+                super.onPostExecute(o);
+            }
+        }.execute();
     }
 }
